b5744350ea26329f91722b9613bf063bce53902a
levi Fitzpatrick <levifitzpatrick1@gmail.com>
2024-12-22 19:50:31 -0500

    stuff on zerrik

--- day-2/main.go
+++ day-2/main.go
@@ -10,58 +10,104 @@ import (
 )
 
 func main() {
-	fileName := "input.txt"
-	total_Bytes, err := os.ReadFile(fileName)
+	fileName := "testinput.txt"
+	totalBytes, err := os.ReadFile(fileName)
 	if err != nil {
 		log.Fatal("Failed to read file "+fileName, err)
 	}
-	total_String := string(total_Bytes)
+	totalString := string(totalBytes)
 
-	rows := strings.Split(total_String, "\n")
+	rows := strings.Split(totalString, "\n")
+	reattempts := []string{}
+	safeCount := 0
+	retryCount := 0
 
-	//m := make(map[int]string)
-	s := 0
-	for _, r := range rows {
-		prev := 0
-		desc := true
-		broken := false
-		for j, num := range strings.Split(r, " ") {
-			if num == "" {
-				continue
-			}
-
-			inum, err := strconv.Atoi(num)
-			if err != nil {
-				log.Fatal("Failed to convert num string to int: ", err)
-			}
-			if j == 0 {
-				prev = inum
-				continue
-			}
-
-			dif := math.Abs(float64(inum - prev))
-			if dif > 3 || dif < 1 {
-				broken = true
-				fmt.Println(fmt.Sprintf("Diff change between %d and %d in row %v", prev, inum, r))
-				break
-			}
-
-			if j == 1 {
-				desc = inum < prev
-			}
-
-			if desc != (inum < prev) {
-				broken = true
-				fmt.Println(fmt.Sprintf("Swapped direction between %d and %d in row %v", prev, inum, r))
-				break
-			}
-
-			prev = inum
+	for _, row := range rows {
+		row = strings.TrimSpace(row)
+		nums := strings.Split(row, " ")
+		if len(nums) < 2 {
+			continue
 		}
-		if broken == false {
-			s += 1
+
+		passed := parseGroup(nums)
+
+		if passed {
+			safeCount++
+		} else {
+			fmt.Println(nums)
+			reattempts = append(reattempts, row)
+			retryCount++
 		}
 	}
-	fmt.Println(s)
 
+	for _, row := range reattempts {
+		row = strings.TrimSpace(row)
+		nums := strings.Split(row, " ")
+		if len(nums) < 2 {
+			continue
+		}
+
+		passed := false
+		for i := 0; i < len(nums); i++ {
+			newNums := remove(nums, i)
+			if len(newNums) < 2 {
+				continue
+			}
+			passed = parseGroup(newNums)
+			if passed {
+				break
+			}
+		}
+
+		if passed {
+			safeCount++
+		}
+	}
+
+	fmt.Println("Retry Count:", retryCount)
+	fmt.Println("Safe Reports with Problem Dampener:", safeCount)
+}
+
+func parseGroup(nums []string) bool {
+	prev := 0
+	desc := true
+	for i, num := range nums {
+		if num == "" {
+			continue
+		}
+		inum, err := strconv.Atoi(num)
+		if err != nil {
+			log.Fatal("Failed to convert num string to int: ", err)
+		}
+		if i == 0 {
+			prev = inum
+			continue
+		}
+		if i == 1 {
+			desc = inum < prev
+		}
+		if inum == prev {
+			fmt.Println(fmt.Sprintf("Current num = prev num at num %d in row %v", inum, nums))
+			return false
+		}
+
+		dif := math.Abs(float64(inum - prev))
+		if dif > 3 || dif < 1 {
+			fmt.Println(fmt.Sprintf("Diff change between %d and %d in row %v", prev, inum, nums))
+			return false
+		}
+
+		if desc != (inum < prev) {
+			fmt.Println(fmt.Sprintf("Swapped direction between %d and %d in row %v", prev, inum, nums))
+			return false
+		}
+
+		prev = inum
+	}
+
+	return true
+}
+
+func remove(slice []string, index int) []string {
+	return append(slice[:index], slice[index+1:]...)
 }
